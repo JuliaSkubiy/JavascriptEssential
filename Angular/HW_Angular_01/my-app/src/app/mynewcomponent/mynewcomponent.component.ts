1c442489c2b74792d295b34ba223bc9f79c160c1
JuliaSkubiy <julia.skubiy@gmail.com>
2019-05-14 01:54:57 +0300

    angular_hw_02

---
+++ Angular/HW_Angular_01/my-app/src/app/mynewcomponent/mynewcomponent.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mynewcomponent',
+  templateUrl: './mynewcomponent.component.html',
+  styleUrls: ['./mynewcomponent.component.css']
+})
+export class MynewcomponentComponent implements OnInit {
+
+  items = ["Hello World", "Привет Мир", "Привіт Світ", "Hola Mundo", "Bonjour le monde", "Hallo Welt", "Ciao mondo", "Witaj świecie", "Hej världen", "Pozdravljen, svet", "Прывітанне Сусвет"];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Angular/HW_Angular_02/hw02/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyTableComponent } from './my-table/my-table.component';
+import { RowComponent } from './my-table/row/row.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyTableComponent,
+    RowComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
